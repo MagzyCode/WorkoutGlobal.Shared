b378b21570a3a0d14de652abfb099131b593fe81
MagzyCode <mihailkazarevich@mail.ru>
2022-11-21 16:32:08 +0300

    Add updation id to update model.

--- WorkoutGlobal.Shared.Messages/UserMessages/UpdateUserMessage.cs
+++ WorkoutGlobal.Shared.Messages/UserMessages/UpdateUserMessage.cs
@@ -1,4 +1,4 @@
 ﻿namespace WorkoutGlobal.Shared.Messages
 {
-    public record UpdateUserMessage(string FirstName, string LastName, string Patronymic);
+    public record UpdateUserMessage(Guid UpdationId, string FirstName, string LastName, string Patronymic);
 }
