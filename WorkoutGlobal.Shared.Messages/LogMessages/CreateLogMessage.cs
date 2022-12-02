f1ab73c03154cbf13095a5ef96ff4770919ac536
MagzyCode <mihailkazarevich@mail.ru>
2022-12-02 11:44:30 +0300

    Update message from class for record.

--- WorkoutGlobal.Shared.Messages/LogMessages/CreateLogMessage.cs
+++ WorkoutGlobal.Shared.Messages/LogMessages/CreateLogMessage.cs
@@ -1,15 +1,4 @@
 ﻿namespace WorkoutGlobal.Shared.Messages
 {
-    public class CreateLogMessage
-    {
-        /// <summary>
-        /// Log info.
-        /// </summary>
-        public string Message { get; set; }
-
-        /// <summary>
-        /// Represents severity id for create relations.
-        /// </summary>
-        public string Severity { get; set; }
-    }
+    public record CreateLogMessage(string Message, string Sevetity);
 }
