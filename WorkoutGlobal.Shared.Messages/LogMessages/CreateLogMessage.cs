d05965afef9df85c0ae6ef60b8c2e4e9dd861938
MagzyCode <mihailkazarevich@mail.ru>
2022-12-02 11:13:42 +0300

    Create message for addind logs.

---
+++ WorkoutGlobal.Shared.Messages/LogMessages/CreateLogMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkoutGlobal.Shared.Messages
+{
+    public class CreateLogMessage
+    {
+        /// <summary>
+        /// Log info.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Represents severity id for create relations.
+        /// </summary>
+        public string Severity { get; set; }
+    }
+}
